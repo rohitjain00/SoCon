20a47426d54f5c00ca215114992eac2c9fbe8bd2
Rohit <rohitjain18005@gmail.com>
2018-10-11 14:39:40 +0530

    db connect created

--- register.php
+++ register.php
@@ -2,17 +2,6 @@
 <html>
 <head>
     <title>User Registration</title>
-<<<<<<< HEAD
-=======
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.4/css/bootstrap.css">
-    <link rel="stylesheet" href="style/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-    <style>
-	body {
-		margin-left : 10px;
-	}
-</style>
->>>>>>> 51b710aea0d49a8074c33df8b7ab190cade36e02
 </head>
 <body>
 <h1>User Registration</h1>
@@ -24,13 +13,12 @@
     <a href="login.php">Login</a>
 </form>
 <?php
+include 'dbConnect.php';
 if(isset($_POST["submit"])){
     if(!empty($_POST['user']) && !empty($_POST['pass'])){
         $user = $_POST['user'];
         $pass = $_POST['pass'];
-        $conn = new mysqli('localhost', 'xrjx', '  ') or die (mysqli_error()); // DB Connection
-        $db = mysqli_select_db($conn, 'test') or die("DB Error"); // Select DB from database
-        //Selecting Database
+
         $query = mysqli_query($conn, "SELECT * FROM userpass WHERE user='".$user."'");
         $numrows = mysqli_num_rows($query);
         if($numrows == 0)
